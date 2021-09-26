4f397ba9f58d4087242df46584dfd178ff699a86
Ankit <ankitdm69@gmail.com>
2021-09-26 12:13:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './src/Home'
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import {
+    Text,
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    StatusBar
+
+} from 'react-native'
+
+
+const Home = () => {
+
+
+    const [randomColor, setRandomColor] = useState('rgb(32, 0, 126)')
+
+    const changeBG = () => {
+     
+        let color = 'rgb(' + 
+        Math.floor(Math.random() * 256) +
+        ',' +
+        Math.floor(Math.random() * 256) +
+        ',' +
+        Math.floor(Math.random() * 256) +
+        ')'
+
+        setRandomColor(color)
+    }
+
+
+return (
+    <>
+    <StatusBar backgroundColor={randomColor} />
+        <View style={[styles.container, { backgroundColor: randomColor }]}>
+            <TouchableOpacity onPress={changeBG}>
+                <Text style={styles.text}>Tap Me</Text>
+            </TouchableOpacity>
+        </View>
+    </>
+)
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        backgroundColor: '#bb2cd9',
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        color: '#ffffff',
+        borderRadius: 15,
+        textTransform: 'uppercase',
+    }
+})
